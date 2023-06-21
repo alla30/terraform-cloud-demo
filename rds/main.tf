784392e62a8ec07bb77d30f92efe68990049d1e4
alla30 <127800418+alla30@users.noreply.github.com>
2023-06-20 20:20:14 -0500

    rds main added

--- rds/main.tf
+++ rds/main.tf
@@ -1,15 +1,15 @@
 data "terraform_remote_state" "vpc" {
-     backend = "remote"
+  backend = "remote"
 
-     config = {
-        organization = "alla30"
-        workspace = {
-            name = "vpc"
-        }
-     }
+  config = {
+    organization = "alla30"
+    workspaces = {
+      name = "vpc"
+    }
+  }
 }
 
-resource "aws_db_subnet_group" "default" { 
-    name = "project-db-subnet-group"
-    subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+resource "aws_db_subnet_group" "default" {
+  name       = "project-db-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
